424397895d97f46bdaad8005a69455ce23a82bcb
igir0006 <igir0006@algonquinlive.com>
2023-11-14 16:33:59 -0500

    Initial Commit

---
+++ GithubTuto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GithubTuto
+//
+//  Created by Berina on 2023-11-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GithubTuto/GithubTutoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTutoApp.swift
+//  GithubTuto
+//
+//  Created by Berina on 2023-11-14.
+//
+
+import SwiftUI
+
+@main
+struct GithubTutoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
